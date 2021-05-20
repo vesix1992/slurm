c959e61086b870628fac7b6169e06e2f76afa0fd
Felip Moll <felip.moll@schedmd.com>
2021-05-20 18:52:35 +0200

    Implement _p_ function for getting/clearing conf

--- src/plugins/cgroup/v1/cgroup_v1.c
+++ src/plugins/cgroup/v1/cgroup_v1.c
@@ -134,11 +134,12 @@ extern bool cgroup_p_has_pid(pid_t pid)
 
 extern void cgroup_p_free_conf(slurm_cgroup_conf_t *cg_conf)
 {
+	cgroup_free_conf(cg_conf);
 }
 
 extern slurm_cgroup_conf_t *cgroup_p_get_conf()
 {
-	return NULL;
+	return cgroup_get_conf();
 }
 
 extern cgroup_limits_t *cgroup_p_root_constrain_get(cgroup_ctl_type_t sub)
