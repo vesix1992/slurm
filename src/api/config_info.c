2e8f61e53e1ec5c9771ea696d529512572df5d26
Alejandro Sanchez <alex@schedmd.com>
2021-06-01 17:23:24 +0200

    Parse, validate and document a new slurm.conf BcastExclude option.
    Bug 11427.

--- src/api/config_info.c
+++ src/api/config_info.c
@@ -648,6 +648,11 @@ extern void *slurm_ctl_conf_2_key_pairs(slurm_conf_t *slurm_ctl_conf_ptr)
 	key_pair->value = xstrdup(tmp_str);
 	list_append(ret_list, key_pair);
 
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("BcastExclude");
+	key_pair->value = xstrdup(slurm_ctl_conf_ptr->bcast_exclude);
+	list_append(ret_list, key_pair);
+
 	slurm_make_time_str((time_t *)&slurm_ctl_conf_ptr->boot_time,
 			    tmp_str, sizeof(tmp_str));
 	key_pair = xmalloc(sizeof(config_key_pair_t));
