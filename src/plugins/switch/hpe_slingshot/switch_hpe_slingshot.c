2dc37787c81312971812033dde4e0fcea49e8955
Tim Wickberg <tim@schedmd.com>
2021-07-12 15:54:39 -0600

    Remove unused functions. Removed upstream by daf93c0357.

--- src/plugins/switch/hpe_slingshot/switch_hpe_slingshot.c
+++ src/plugins/switch/hpe_slingshot/switch_hpe_slingshot.c
@@ -489,22 +489,6 @@ error:
 	return SLURM_ERROR;
 }
 
-void switch_p_print_jobinfo(FILE *fp, switch_jobinfo_t *jobinfo)
-{
-	return;
-}
-
-char *switch_p_sprint_jobinfo(switch_jobinfo_t *switch_jobinfo, char *buf,
-		size_t size)
-{
-	if ((buf != NULL) && size) {
-		buf[0] = '\0';
-		return buf;
-	}
-
-	return NULL;
-}
-
 /*
  * switch functions for job initiation
  */
